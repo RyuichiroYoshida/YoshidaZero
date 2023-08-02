df1b38ef249264d05018854bc30f7be2ad28b3c4
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-08-02 12:46:31 +0900

    [add] Stateパターン枠組み完成

---
+++ Assets/Scripts/IdleState.cs
@@ -0,0 +1,24 @@
+public class IdleState : IState
+{
+    private PlayerController player;
+
+    public IdleState (PlayerController player)
+    {
+        this.player = player;
+    }
+    public void Enter()
+    {
+        // 最初に状態に入ったときに実行されるコード
+    }
+
+    public void Update()
+    {
+        // ここに、別の状態に遷移するための条件が成立しているかどうかを
+        // 検出するためのロジックを追加する
+    }
+
+    public void Exit()
+    {
+        // 状態を抜けるときに実行されるコード
+    }
+}
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -5,4 +5,33 @@ public class StateMachine
 {
     public IState CurrentState { get; private set; }
 
+    public WalkState walkState;
+    public JumpState jumpState;
+    public IdleState idleState;
+
+    public StateMachine(PlayerController player)
+    {
+        this.walkState = new WalkState(player);
+        this.jumpState = new JumpState(player);
+        this.idleState = new IdleState(player);
+    }
+
+    public void Initialize(IState startingState)
+    {
+        CurrentState = startingState;
+        startingState.Enter();
+    }
+
+    public void TransitionTo(IState nextState)
+    {
+        CurrentState.Exit();
+        CurrentState = nextState;
+        nextState.Enter();
+    }
+
+    public void Update()
+    {
+        CurrentState?.Update();
+    }
+
 }
