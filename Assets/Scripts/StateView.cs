b46e45d924d597f5a6500d78ba2bae43c8f32e75
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-08-11 23:47:17 +0900

    [update] 現在のStateを表示

--- Assets/Scripts/StateView.cs
+++ Assets/Scripts/StateView.cs
@@ -4,15 +4,20 @@ using UnityEngine.UI;
 public class StateView : MonoBehaviour
 {
     [SerializeField] Text _stateViewText;
-
+    private GameObject _player;
     private PlayerController _playerController;
+    private StateMachine _playerStateMachine;
 
     void Start()
     {
-            
+        _player = GameObject.FindWithTag("Player").gameObject;
+        _playerController = _player.GetComponent<PlayerController>();
+        _playerStateMachine = _playerController.PlayerStateMachine;
+        _playerStateMachine.StateChanged += View;
     }
-    void Update()
+
+    void View(IState state)
     {
-        _stateViewText.text = _playerController.PlayerStateMachine.CurrentState.ToString();
+        _stateViewText.text = state?.GetType().Name;
     }
 }
