a60888d60edf4f2a453c3e98cfe0054084a1081e
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-08-17 13:11:34 +0900

    [update] アニメーション

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -28,6 +26,8 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void KillCount()
+    {
 
-
+    }
 }
