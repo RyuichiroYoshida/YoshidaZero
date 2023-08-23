0d953195b0b2526d5e7bef940861370b35a9ad99
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-08-23 22:51:06 +0900

    [add] 斬撃パラメータ選択

---
+++ Assets/Scripts/ZangekiController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class ZangekiController : MonoBehaviour
+{
+    [SerializeField] float _zangekiSpeed = 5f;
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
