f78b38d3e667c2619606ea15be9e2bcc6efde2d4
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-09-02 20:15:26 +0900

    feat: シーン遷移追加

--- Assets/Scripts/StageManager/StageManager.cs
+++ Assets/Scripts/StageManager/StageManager.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class StageManager : MonoBehaviour
 {
     [SerializeField] bool _stageClear = false;
+    [SerializeField] string _sceneName;
     public bool StageClear => _stageClear;
+    /// <summary>
+    /// 引数のシーン名に遷移します
+    /// </summary>
+    /// <param name="sceneName">シーン名</param>
+    public void SceneChange(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             _stageClear = true;
+            SceneChange(_sceneName);
         }
     }
 }
