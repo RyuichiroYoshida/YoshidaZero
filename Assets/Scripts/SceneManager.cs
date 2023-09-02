f98fdc14250f6d2d12a091a02dcce5f6f6ec88ba
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-09-03 02:37:48 +0900

    feat: シーン遷移時のフェードアウト追加

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField] Image _fade;
+
     /// <summary>
     /// 引数のシーン名に遷移します
     /// </summary>
     /// <param name="sceneName">シーン名</param>
     public void SceneChange(string sceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        _fade.DOFade(0, 1f).onComplete = () => UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
