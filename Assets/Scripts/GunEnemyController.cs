a445f5e72ebffc7bda16f21e41d2318b2fa7ea64
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-09-09 00:41:32 +0900

    feat: RayCastの壁衝突時反転機能作成

--- Assets/Scripts/GunEnemyController.cs
+++ Assets/Scripts/GunEnemyController.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class GunEnemyController : EnemyBase
 {
     [SerializeField] float _detectionRange;
+    [SerializeField] float _moveSpeed;
     [SerializeField] GameObject _bulletPrefab;
     [SerializeField] LayerMask _wallLayer;
     [SerializeField] LayerMask _playerLayer;
-    bool _hitRaycast = false;
-    bool _hitWall = false;
+    float _flipValue = 1;
     RaycastHit2D _wallHit;
     RaycastHit2D _playerHit;
     void Start()
@@ -19,20 +19,11 @@ public class GunEnemyController : EnemyBase
     {
         if (_wallHit)
         {
-            _hitRaycast = !_hitRaycast;
             print("Wall Hit");
+            _flipValue *= -1;
         }
-
-        if (_hitRaycast)
-        {
-            EnemyMove(-1);
-            EnemyAttack(-1);
-        }
-        else
-        {
-            EnemyMove(1);
-            EnemyAttack(1);
-        }
+        EnemyMove(_flipValue);
+        EnemyAttack(_flipValue);
     }
 
     void EnemyMove(float value)
@@ -40,15 +31,18 @@ public class GunEnemyController : EnemyBase
         Vector2 _wallLineStart = new Vector2(transform.position.x * value, transform.position.y - 0.5f);
         Vector2 _wallLineEnd = new Vector2((transform.position.x + 3) * value, transform.position.y - 0.5f);
         _wallHit = Physics2D.Linecast(_wallLineStart, _wallLineEnd, _wallLayer);
-        if (_wallHit)  print("Wall Hit");
+        if (_wallHit)
+            print("Wall Hit");
         Debug.DrawLine(_wallLineStart, _wallLineEnd, Color.blue);
     }
 
     void EnemyAttack(float value)
     {
-        Vector2 _playerLineStart = new Vector2(transform.position.x + 1 * value, transform.position.y);
-        Vector2 _playerLineEnd = new Vector2(transform.position.x + _detectionRange * value, transform.position.y);
+        Vector2 _playerLineStart = new Vector2((transform.position.x + 1) * value, transform.position.y);
+        Vector2 _playerLineEnd = new Vector2((transform.position.x + _detectionRange) * value, transform.position.y);
         _playerHit = Physics2D.Linecast(_playerLineStart, _playerLineEnd, _playerLayer);
+        if (_playerHit)
+            print("Player Hit");
         Debug.DrawLine(_playerLineStart, _playerLineEnd, Color.red);
     }
 }
