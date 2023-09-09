bd9d49a5c21d45ec82a86492301dfc465728bbdc
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-09-09 14:59:06 +0900

    feat: GunEnemyのプレイヤー検知作成
    壁を検知すると反転し、プレイヤーを検知すると停止する

--- Assets/Scripts/GunEnemyController.cs
+++ Assets/Scripts/GunEnemyController.cs
@@ -7,12 +7,14 @@ public class GunEnemyController : EnemyBase
     [SerializeField] GameObject _bulletPrefab;
     [SerializeField] LayerMask _wallLayer;
     [SerializeField] LayerMask _playerLayer;
-    float _flipValue = 1;
-    RaycastHit2D _wallHit;
-    RaycastHit2D _playerHit;
+    [SerializeField, Tooltip("デバッグ用")] float _flipValue = 1;
+    [SerializeField, Tooltip("デバッグ用")] RaycastHit2D _wallHit;
+    [SerializeField, Tooltip("デバッグ用")] RaycastHit2D _playerHit;
+    Rigidbody2D _rb;
+
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -26,23 +28,27 @@ public class GunEnemyController : EnemyBase
         EnemyAttack(_flipValue);
     }
 
+    /// <summary>
+    /// エネミーの壁検知ラインキャストと移動管理
+    /// </summary>
+    /// <param name="value">移動方向の転換の引数</param>
     void EnemyMove(float value)
     {
-        Vector2 _wallLineStart = new Vector2(transform.position.x * value, transform.position.y - 0.5f);
-        Vector2 _wallLineEnd = new Vector2((transform.position.x + 3) * value, transform.position.y - 0.5f);
-        _wallHit = Physics2D.Linecast(_wallLineStart, _wallLineEnd, _wallLayer);
-        if (_wallHit)
-            print("Wall Hit");
-        Debug.DrawLine(_wallLineStart, _wallLineEnd, Color.blue);
+        // 壁検知用ラインキャスト
+        Vector2 _wallLineEnd = new Vector2(transform.position.x + (2 * value), transform.position.y - 0.5f);
+        _wallHit = Physics2D.Linecast(transform.position, _wallLineEnd, _wallLayer);
+        // 移動、壁に当たると反転
+        _rb.velocity = Vector2.right * _moveSpeed * value;
+        // ラインキャストを可視化
+        Debug.DrawLine(transform.position, _wallLineEnd, Color.blue);
     }
 
     void EnemyAttack(float value)
     {
-        Vector2 _playerLineStart = new Vector2((transform.position.x + 1) * value, transform.position.y);
-        Vector2 _playerLineEnd = new Vector2((transform.position.x + _detectionRange) * value, transform.position.y);
-        _playerHit = Physics2D.Linecast(_playerLineStart, _playerLineEnd, _playerLayer);
+        Vector2 _playerLineEnd = new Vector2(transform.position.x + (_detectionRange * value), transform.position.y);
+        _playerHit = Physics2D.Linecast(transform.position, _playerLineEnd, _playerLayer);
         if (_playerHit)
-            print("Player Hit");
-        Debug.DrawLine(_playerLineStart, _playerLineEnd, Color.red);
+            this._rb.velocity = Vector2.zero;
+        Debug.DrawLine(transform.position, _playerLineEnd, Color.red);
     }
 }
