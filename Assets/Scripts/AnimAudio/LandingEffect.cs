b44cedd861481147d916e4f5ef17d7efca47044c
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-05 19:26:20 +0900

    feat: 着地時エフェクト追加

---
+++ Assets/Scripts/AnimAudio/LandingEffect.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class LandingEffect : StateMachineBehaviour
+{
+    [SerializeField] GameObject _laundingPrefab;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        GameObject.FindGameObjectWithTag("Player").TryGetComponent<Transform>(out var transform);
+        var position = transform.position;
+        Instantiate(_laundingPrefab, new Vector2(position.x, position.y + -0.7f), Quaternion.identity);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
